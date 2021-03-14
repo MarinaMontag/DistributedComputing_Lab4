cd2b49ed8a400b2087072730cc26e78ef660a91c
Marina Montag <montag.marinka@gmail.com>
2021-03-14 17:45:10 +0200

    finished

--- Lab4_3/main.go
+++ Lab4_3/main.go
@@ -1,16 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"sync"
+	"time"
 )
 
-func main() {
+var queueOfCitiesToAdd []*City
+
+func createCity(value string) *City {
+	return &City{value, false}
+}
+
+func createQueueOfCities() {
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("A"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("B"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("C"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("D"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("E"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("G"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("H"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("I"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("J"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("K"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("L"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("M"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("N"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("O"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("P"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("Q"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("Y"))
+	queueOfCitiesToAdd = append(queueOfCitiesToAdd, createCity("Z"))
 
 }
 
+func main() {
+	airport := createAirport()
+	createQueueOfCities()
+	airport.wg.Add(4)
+	go AddDeleteCities(airport)
+	go AddDeleteFlights(airport)
+	go ChangePrice(airport)
+	go SearchRouteFromAtoB(airport)
+	airport.wg.Wait()
+}
+
 type City struct {
-	name string
+	name    string
+	visited bool
 }
 
 type Flight struct {
@@ -19,52 +57,160 @@ type Flight struct {
 }
 
 type Airport struct {
+	wg      *sync.WaitGroup
 	cities  []*City
 	flights map[*City][]*Flight
-	lock    sync.RWMutex
+	lock    *sync.RWMutex
+}
+
+func createAirport() *Airport {
+	var rwLock sync.RWMutex
+	var wg sync.WaitGroup
+	return &Airport{&wg, nil, make(map[*City][]*Flight), &rwLock}
+}
+
+func (graph *Airport) display() {
+	for _, city := range graph.cities {
+		fmt.Print(city.name + "->")
+		if len(graph.flights[city]) == 0 {
+			fmt.Println()
+		}
+		for j, flight := range graph.flights[city] {
+			fmt.Printf("%v {%v}", flight.direction.name, flight.price)
+			if j == len(graph.flights[city])-1 {
+				fmt.Println()
+			} else {
+				fmt.Print(", ")
+			}
+		}
+	}
+	fmt.Println()
 }
 
 //потік, що змінює ціну квитка;
-
-func (graph *Airport) changePrice() {
-	newPrice := (rand.Intn(20) + 1) * 100
-	graph.lock.RLock()
-	A := rand.Intn(len(graph.cities))
-	cityA := graph.cities[A]
-	B := rand.Intn(len(graph.flights[cityA]))
-	cityB := graph.flights[cityA][B].direction
-	graph.lock.Lock()
-	graph.flights[cityA][B].price = newPrice
-	for i, flight := range graph.flights[cityB] {
-		if flight.direction == cityA {
-			graph.flights[cityB][i].price = newPrice
+func ChangePrice(graph *Airport) {
+	defer graph.wg.Done()
+	for {
+		graph.lock.Lock()
+		if len(queueOfCitiesToAdd) > 0 {
+			if len(graph.cities) > 1 {
+				graph.changePrice()
+				graph.lock.Unlock()
+				time.Sleep(300 * time.Millisecond)
+			} else {
+				graph.lock.Unlock()
+			}
+		} else {
+			graph.lock.Unlock()
 			break
 		}
 	}
-	graph.lock.Unlock()
-	graph.lock.RUnlock()
+}
+
+func (graph *Airport) changePrice() {
+
+	A := rand.Intn(len(graph.cities))
+	cityA := graph.cities[A]
+	sizeOfFlights := len(graph.flights[cityA])
+
+	if sizeOfFlights > 0 {
+		fmt.Println("Change price:")
+		B := rand.Intn(sizeOfFlights)
+		cityB := graph.flights[cityA][B].direction
+		var newPrice int
+		for {
+			newPrice = (rand.Intn(20) + 1) * 100
+			if newPrice != graph.flights[cityA][B].price {
+				graph.flights[cityA][B].price = newPrice
+				break
+			}
+		}
+
+		for i, flight := range graph.flights[cityB] {
+			if flight.direction == cityA {
+				graph.flights[cityB][i].price = newPrice
+				break
+			}
+		}
+		graph.display()
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////
 
 //потік, що видаляє і додає рейси між містами;
 
-func (graph *Airport) AddFlight(price int, cityA *City, cityB *City) {
-	graph.lock.Lock()
-	flight1 := Flight{price, cityA}
-	flight2 := Flight{price, cityB}
-	graph.flights[cityA] = append(graph.flights[cityA], &flight2)
-	graph.flights[cityB] = append(graph.flights[cityB], &flight1)
-	graph.lock.Unlock()
+func AddDeleteFlights(graph *Airport) {
+	defer graph.wg.Done()
+	for {
+		graph.lock.Lock()
+		if len(queueOfCitiesToAdd) > 0 {
+			if len(graph.cities) > 1 {
+				random := rand.Intn(3)
+				if random >= 0 && random < 2 {
+					graph.AddFlight()
+				} else {
+					graph.DeleteFlight()
+				}
+			}
+			graph.lock.Unlock()
+			time.Sleep(100 * time.Millisecond)
+		} else {
+			graph.lock.Unlock()
+			break
+		}
+	}
 }
 
-func (graph *Airport) DeleteFlight(A int, B int) {
-	graph.lock.Lock()
+func getB(A int, graph *Airport) int {
+	var B int
+	for {
+		B = rand.Intn(len(graph.cities))
+		if B != A {
+			return B
+		}
+	}
+}
+
+func (graph *Airport) AddFlight() {
+	A := rand.Intn(len(graph.cities))
 	cityA := graph.cities[A]
-	cityB := graph.cities[B]
-	graph.deleteDirection(cityA, cityB)
-	graph.deleteDirection(cityB, cityA)
-	graph.lock.Unlock()
+	var B int
+	var cityB *City
+	B = getB(A, graph)
+	cityB = graph.cities[B]
+	if !cityA.containsFlightTo(cityB, graph) {
+		fmt.Println("Add flight")
+		price := (rand.Intn(20) + 1) * 100
+		flight1 := Flight{price, cityA}
+		flight2 := Flight{price, cityB}
+		graph.flights[cityA] = append(graph.flights[cityA], &flight2)
+		graph.flights[cityB] = append(graph.flights[cityB], &flight1)
+		graph.display()
+	}
+}
+
+func (cityA *City) containsFlightTo(cityB *City, graph *Airport) bool {
+	for _, dir := range graph.flights[cityA] {
+		if dir.direction == cityB {
+			return true
+		}
+	}
+	return false
+}
+
+func (graph *Airport) DeleteFlight() {
+	A := rand.Intn(len(graph.cities))
+	cityA := graph.cities[A]
+	sizeOfDirections := len(graph.flights[cityA])
+	if sizeOfDirections > 0 {
+		fmt.Println("Delete Flight:")
+		B := rand.Intn(sizeOfDirections)
+		cityB := graph.flights[cityA][B].direction
+		graph.deleteDirection(cityA, cityB)
+		graph.deleteDirection(cityB, cityA)
+		graph.display()
+	}
 }
 
 func (graph *Airport) deleteDirection(cityA *City, cityB *City) {
@@ -82,23 +228,45 @@ func (graph *Airport) deleteDirection(cityA *City, cityB *City) {
 
 //потік, що видаляє старі міста і додає нові;
 
+func AddDeleteCities(graph *Airport) {
+	defer graph.wg.Done()
+	for {
+		graph.lock.Lock()
+		if len(queueOfCitiesToAdd) > 0 {
+			random := rand.Intn(3)
+			if random >= 0 && random < 2 {
+				cityIndex := rand.Intn(len(queueOfCitiesToAdd))
+				cityToAdd := queueOfCitiesToAdd[cityIndex]
+				queueOfCitiesToAdd = append(queueOfCitiesToAdd[:cityIndex], queueOfCitiesToAdd[cityIndex+1:]...)
+				graph.AddCity(cityToAdd)
+			} else {
+				graph.DeleteCity()
+			}
+		} else {
+			graph.lock.Unlock()
+			break
+		}
+		graph.lock.Unlock()
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
 func (graph *Airport) AddCity(city *City) {
-	graph.lock.Lock()
+	fmt.Println("Add city:")
 	graph.flights[city] = nil
 	graph.cities = append(graph.cities, city)
-	graph.lock.Unlock()
+	graph.display()
 }
 
 func (graph *Airport) DeleteCity() {
-	graph.lock.Lock()
 	deletedCity := graph.deleteNode()
 	if deletedCity == nil {
-		defer graph.lock.Unlock()
 		return
+	} else {
+		fmt.Println("Delete city")
 	}
 	graph.deleteFlightsToDeletedCity(deletedCity)
-	graph.lock.Unlock()
-
+	graph.display()
 }
 
 func (graph *Airport) deleteNode() *City {
@@ -132,8 +300,77 @@ func (graph *Airport) deleteFlightsToDeletedCity(deletedCity *City) {
 //потоки, що визначають чи є шлях від довільного міста А до довільного міста Б,
 //і яка ціна такої поїздки (якщо прямого шляху немає, то знайти будь-який шлях з існуючих)
 
-func (graph *Airport) searchFlight(cityA *City, cityB *City) {
+func SearchRouteFromAtoB(graph *Airport) {
+	defer graph.wg.Done()
+	for {
+		graph.lock.RLock()
+		if len(queueOfCitiesToAdd) > 0 {
+			if len(graph.cities) > 1 {
+				i := 0
+				head := graph.cities[rand.Intn(len(graph.cities))]
+				var destination *City
+				for {
+					destination = graph.cities[rand.Intn(len(graph.cities))]
+					if destination != head {
+						break
+					}
+				}
+				price := 0
+				found := false
+				fmt.Println("Search route from city " + head.name + " to " + destination.name + ":")
+				graph.searchRoute(&i, head, nil, head, destination, &price, &found)
+				graph.makeCitiesNonVisited()
+				if found {
+					fmt.Printf("Price of such route costs %v\n\n", price)
+				} else {
+					fmt.Println("Such route doesn't exist")
+					fmt.Println()
+				}
+				graph.lock.RUnlock()
+				time.Sleep(300 * time.Millisecond)
+			} else {
+				graph.lock.RUnlock()
+				time.Sleep(100 * time.Millisecond)
+			}
+		} else {
+			graph.lock.RUnlock()
+			break
+		}
+	}
+}
 
+func (graph *Airport) searchRoute(i *int, head *City, prev *City, current *City, destination *City, price *int, found *bool) {
+	if *i > 0 && current == head || current.visited {
+		return
+	}
+	temp := *price
+	for j, flight := range graph.flights[current] {
+		if !*found {
+			if j == len(graph.flights[current])-1 {
+				current.visited = true
+			}
+			*price = temp
+			if flight.direction == destination {
+				*price += flight.price
+				*found = true
+				return
+			} else if flight.direction == prev {
+				continue
+			} else {
+				*i++
+				*price += flight.price
+				graph.searchRoute(i, head, current, flight.direction, destination, price, found)
+			}
+		} else {
+			break
+		}
+	}
+}
+
+func (graph *Airport) makeCitiesNonVisited() {
+	for _, city := range graph.cities {
+		city.visited = false
+	}
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
